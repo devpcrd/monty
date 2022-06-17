95462145b14b90e8b51ef5ca73ba3e27b257e916
devpcrd <infinitepeco@gmail.com>
2022-06-17 09:20:31 +0530

    revamp exercise

--- main.c
+++ main.c
@@ -1,114 +1,48 @@
 #include "monty.h"
-
-char stack_queue = 's';
-
 /**
- * main - Entry point
- * @argc: number of arguments passed as parameter to main program.
- * @argv: array of strings with the parameters passed to main program
- *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on any failure.
+ * make_struct - Set values for struct.
+ * @argv: Argument list.
+ * Return: Struct for buffers.
+ */
+buf_struct *make_struct(char *argv[])
+{
+	static buf_struct a;
+
+	a.argv = argv;
+	memset(a.read_buff, 0, sizeof(a.read_buff));
+	memset(a.list_cmd, 0, sizeof(a.list_cmd));
+	memset(a.tok_cmd, 0, sizeof(a.tok_cmd));
+
+	return (&a);
+}
+/**
+ * main - Takes argument list and executes file given.
+ * @argc: Argument count.
+ * @argv: List of arguments.
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-	FILE *stream = NULL;
-	size_t len = 0;
-	unsigned int line_number = 1;
-	char *line = NULL;
-	char *code = NULL;
-	stack_t *stack = NULL;
+	int fd;
+	buf_struct *a;
+
+	a = make_struct(argv);
 
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	while ((fd = open(argv[1], O_RDONLY)) == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
-	stream = fopen(argv[1], "r");
-	if (stream == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+	read(fd, a->read_buff, 4096);
+	close(fd);
+	split_newline(a);
+	exec_loop(a);
 
-	while (getline(&line, &len, stream) != -1)
-	{
-		code = strtok(line, " \t\r\n\v\f");
-		if (code != NULL && code[0] != '#')
-			get_opcode(&stack, line_number, code);
-		line_number++;
-	}
-
-	free_stack_t(stack);
-	stack = NULL;
-	free(line);
-	fclose(stream);
-	exit(EXIT_SUCCESS);
-}
-
-/**
- * get_opcode - reads opcode and verifies if is valid.
- * @stack: double pointer to header (top) of the stack.
- * @line_number: counter for line number of the file.
- * @code: opcode to excecute.
- *
- * Return: void.
- */
-void get_opcode(stack_t **stack, unsigned int line_number, char *code)
-{
-	int i = 0;
-	instruction_t opcode_func[] = {
-		{"add", _add},
-		{"div", _div},
-		{"mod", _mod},
-		{"mul", _mul},
-		{"nop", _nop},
-		{"pall", _pall},
-		{"pint", _pint},
-		{"pop", _pop},
-		{"push", _push},
-		{"sub", _sub},
-		{"swap", _swap},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotr", _rotr},
-		{"rotl", _rotl},
-		{"stack", _stack},
-		{"queue", _queue},
-		{NULL, NULL}
-	};
-
-	while (opcode_func[i].opcode)
-	{
-		if (strcmp(opcode_func[i].opcode, code) == 0)
-		{
-			opcode_func[i].f(stack, line_number);
-			return;
-		}
-		i++;
-	}
-	dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, code);
-	free_stack_t(*stack);
-	/**
-	 * close_file
-	 */
-	exit(EXIT_FAILURE);
-}
-
-/**
- * free_stack_t - function that free a list of type dlistint_t
- * @head: pointer to a list type stack_t
- *
- * Return: void.
- */
-void free_stack_t(stack_t *head)
-{
-	stack_t *temp;
-
-	while (head != NULL)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+	return (0);
 }
--- monty.h
+++ monty.h
@@ -1,8 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
-
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -10,13 +16,13 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -25,40 +31,78 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
-/* Function Declarations */
-void _add(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
-void _mod(stack_t **stack, unsigned int line_number);
-void _mul(stack_t **stack, unsigned int line_number);
-void _nop(stack_t **stack, unsigned int line_number);
-void _pall(stack_t **stack, unsigned int line_number);
-void _pint(stack_t **stack, unsigned int line_number);
-void _pop(stack_t **stack, unsigned int line_number);
-void _push(stack_t **stack, unsigned int line_number);
-void _sub(stack_t **stack, unsigned int line_number);
-void _swap(stack_t **stack, unsigned int line_number);
-void _pchar(stack_t **stack, unsigned int line_number);
-void _pstr(stack_t **stack, unsigned int line_number);
-void _rotr(stack_t **stack, unsigned int line_number);
-void _rotl(stack_t **stack, unsigned int line_number);
-void get_opcode(stack_t **stack, unsigned int line_number, char *code);
-char **token_opcode(char *line);
-void free_stack_t(stack_t *head);
-void add_node(stack_t **stack, int argument);
-void add_node_queue(stack_t **stack, int argument);
+/**
+ * struct buf_struct - Global buffers.
+ * @read_buff: Buffer for read.
+ * @list_cmd: Tokenized read buffer.
+ * @tok_cmd: Tokenize each token from previous split based on spaces.
+ * @argv: Arguments from command line.
+ *
+ * Description: - Buffers used globally throughout files.
+ */
+typedef struct buf_struct
+{
+	char read_buff[4096];
+	char *list_cmd[1000];
+	char *tok_cmd[1000];
+	char **argv;
+} buf_struct;
 
 
-/*extern global variable */
-extern char stack_queue;
+/* Execute functions */
 
+void (*get_op_func(char *s))(stack_t **stack, unsigned int line_number);
+void exec_loop(buf_struct *a);
 
-#endif
+/* End of execute functions */
+
+/* String functions */
+
+char **split_spaces(char *buff, buf_struct *a);
+char **split_newline(buf_struct *a);
+buf_struct *make_struct(char *argv[]);
+
+/*End of string functions */
+
+/* Doubly linked list functions */
+
+void free_stack(stack_t *head);
+
+/* End of Doubly linked list functions */
+
+/* Helper functions */
+
+int digits_only(char *str);
+
+/* End of helper functions */
+
+/* monty functions */
+
+stack_t *push(stack_t **head, int n);
+void pall(stack_t **h, unsigned int line_n);
+void pint(stack_t **h, unsigned int line_n);
+void pop(stack_t **h, unsigned int line_n);
+void swap(stack_t **stack, unsigned int line_n);
+void add(stack_t **stack, unsigned int line_n);
+void sub(stack_t **stack, unsigned int line_n);
+void _div(stack_t **stack, unsigned int line_n);
+void mod(stack_t **stack, unsigned int line_n);
+void mul(stack_t **stack, unsigned int line_n);
+void nop(stack_t **stack, unsigned int line_n);
+void pchar(stack_t **stack, unsigned int line_n);
+void pstr(stack_t **stack, unsigned int line_n);
+void rotl(stack_t **stack, unsigned int line_n);
+void rotr(stack_t **stack, unsigned int line_n);
+void queue(stack_t **stack, unsigned int line_n);
+
+/* end of monty functions */
+
+#endif /*MONTY_H*/
